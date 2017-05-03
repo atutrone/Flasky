cda2748255148fe13668c22671e3e0b690dddea1
atutrone <anthony@tutrone.net>
2017-05-02 22:42:11 -0400

    Started working through databases and adding creation/connectivity to Flask via SQLAlchemy

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 import os
-from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, session, redirect, url_for
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
+from flask_wtf import FlaskForm
+from flask_migrate import Migrate, MigrateCommand
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,12 +16,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ThisIsNotSecure'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+manager = Manager(app)
 db = SQLAlchemy(app)
-bootstap = Bootstrap(app)
+bootstrap = Bootstrap(app)
 moment = Moment(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField ('What is your name?', validators=[Required()])
     #"Required" sets the string field to be needed in order for the form to submit successfully.  Will return error that the field is empty.
     submit = SubmitField('Submit')
@@ -28,7 +34,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -42,17 +48,25 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you've changed your name!")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form = form, name = session.get('name'), known = session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
