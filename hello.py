978abc969c93732fce5df6e9fdb5035f698ef4fd
atutrone <anthony@tutrone.net>
2017-05-03 22:50:53 -0400

    Started working through databases and adding creation/connectivity to Flask via SQLAlchemy

--- hello.py
+++ hello.py
@@ -9,6 +9,7 @@ from flask_migrate import Migrate, MigrateCommand
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,18 +17,30 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ThisIsNotSecure'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#mail configs:
+app.config['MAIL_SERVER'] = '127.0.0.1'
+app.config['MAIL_PORT'] = 1025
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 manager = Manager(app)
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
+mail = Mail(app)
+
 
 class NameForm(FlaskForm):
     name = StringField ('What is your name?', validators=[Required()])
-    #"Required" sets the string field to be needed in order for the form to submit successfully.  Will return error that the field is empty.
+    #"Required" sets the string field to be needed in order for the form to submit successfully.
+    # Will return error that the field is empty.
     submit = SubmitField('Submit')
 
 class Role(db.Model):
@@ -51,26 +64,14 @@ class User(db.Model):
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username = form.name.data)
-            db.session.add(user)
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'), known = session.get('known', False))
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+def send_email(to, subject, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = 'Hello.py test'
+    msg.html = '<b>HTML</b> Test from hello.py'
+    mail.send(msg)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -80,5 +81,23 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 if __name__ == '__main__':
+    #manager.run()
     app.run(debug=True, port=8080)
